296d29172b0b4f124fb941e2dbbd75adaf96266b
vitov <vitovt.kublitsky@mail.ru>
2021-01-30 14:47:21 +0300

    HW08 model refactoring

--- src/ru/javawebinar/basejava/model/Organization.java
+++ src/ru/javawebinar/basejava/model/Organization.java
@@ -10,20 +10,8 @@ import java.util.Objects;
 public class Organization {
     private final Link homePage;
 
-    private final LocalDate startDate;
-    private final LocalDate endDate;
-    private final String title;
-    private final String description;
-
-    public Organization(String name, String url, LocalDate startDate, LocalDate endDate, String title, String description) {
-        Objects.requireNonNull(startDate, "startDate must not be null");
-        Objects.requireNonNull(endDate, "endDate must not be null");
-        Objects.requireNonNull(title, "title must not be null");
+    public Organization(String name, String url) {
         this.homePage = new Link(name, url);
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.title = title;
-        this.description = description;
     }
 
     @Override
@@ -33,32 +21,18 @@ public class Organization {
 
         Organization that = (Organization) o;
 
-        if (!homePage.equals(that.homePage)) return false;
-        if (!startDate.equals(that.startDate)) return false;
-        if (!endDate.equals(that.endDate)) return false;
-        if (!title.equals(that.title)) return false;
-        return description != null ? description.equals(that.description) : that.description == null;
+        return homePage.equals(that.homePage);
 
     }
 
     @Override
     public int hashCode() {
         int result = homePage.hashCode();
-        result = 31 * result + startDate.hashCode();
-        result = 31 * result + endDate.hashCode();
-        result = 31 * result + title.hashCode();
-        result = 31 * result + (description != null ? description.hashCode() : 0);
         return result;
     }
 
     @Override
     public String toString() {
-        return "Organization{" +
-                "homePage=" + homePage +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                '}';
+        return homePage.toString();
     }
 }
--- src/ru/javawebinar/basejava/model/OrganizationSection.java
+++ src/ru/javawebinar/basejava/model/OrganizationSection.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.basejava.model;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
@@ -8,15 +9,15 @@ import java.util.Objects;
  * 19.07.2016
  */
 public class OrganizationSection extends Section {
-    private final List<Organization> organizations;
+    private final List<Participation> participationList;
 
-    public OrganizationSection(List<Organization> organizations) {
-        Objects.requireNonNull(organizations, "organizations must not be null");
-        this.organizations = organizations;
+    public OrganizationSection(List<Participation> participations) {
+        Objects.requireNonNull(participations, "participationList must not be null");
+        this.participationList = participations;
     }
 
-    public List<Organization> getOrganizations() {
-        return organizations;
+    public List<Participation> getParticipationList() {
+        return participationList;
     }
 
     @Override
@@ -26,17 +27,73 @@ public class OrganizationSection extends Section {
 
         OrganizationSection that = (OrganizationSection) o;
 
-        return organizations.equals(that.organizations);
+        return participationList.equals(that.participationList);
 
     }
 
     @Override
     public int hashCode() {
-        return organizations.hashCode();
+        return participationList.hashCode();
     }
 
     @Override
     public String toString() {
-        return organizations.toString();
+        return participationList.toString();
+    }
+
+    private class Participation {
+        final Organization organization;
+        final LocalDate startDate;
+        final LocalDate endDate;
+        final String title;
+        final String description;
+
+        public Participation(Organization organization, LocalDate startDate, LocalDate endDate, String title, String description) {
+            Objects.requireNonNull(organization, "organisation must not be null");
+            Objects.requireNonNull(startDate, "startDate must not be null");
+            Objects.requireNonNull(endDate, "endDate must not be null");
+            Objects.requireNonNull(title, "title must not be null");
+            this.organization = organization;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.title = title;
+            this.description = description;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Participation that = (Participation) o;
+
+            if (!organization.equals(that.organization)) return false;
+            if (!startDate.equals(that.startDate)) return false;
+            if (!endDate.equals(that.endDate)) return false;
+            if (!title.equals(that.title)) return false;
+            return description != null ? description.equals(that.description) : that.description == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = organization.hashCode();
+            result = 31 * result + startDate.hashCode();
+            result = 31 * result + endDate.hashCode();
+            result = 31 * result + title.hashCode();
+            result = 31 * result + (description != null ? description.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Organization{" +
+                    "organisation=" + organization +
+                    ", startDate=" + startDate +
+                    ", endDate=" + endDate +
+                    ", title='" + title + '\'' +
+                    ", description='" + description + '\'' +
+                    '}';
+        }
     }
 }
