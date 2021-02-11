38461569c5c53b0d52151c07d925ede20c8ac024
vitov <vitovt.kublitsky@mail.ru>
2021-02-11 13:14:48 +0300

    HW13 SqlStorage realisation

--- src/ru/javawebinar/basejava/sql/SqlHelper.java
+++ src/ru/javawebinar/basejava/sql/SqlHelper.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class SqlHelper<T> {
+public class SqlHelper {
 
     private final ConnectionFactory connectionFactory;
 
@@ -16,25 +16,34 @@ public class SqlHelper<T> {
         this.connectionFactory = connectionFactory;
     }
 
-    private <R> R processConnection(String sql, StatementProcessor<R> statementProcessor, Object... params) {
+    private <R> R processConnection(String sql, StatementProcessor<R> statementProcessor, Object... params) throws SQLException {
         try (Connection conn = connectionFactory.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             setParams(ps, params);
             return statementProcessor.process(ps);
+        }
+    }
+
+    public int count() {
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement("SELECT count(*) FROM resume")) {
+            ResultSet resultSet = ps.executeQuery();
+            resultSet.next();
+            return resultSet.getInt(1);
         } catch (SQLException e) {
             throw new StorageException(e);
         }
     }
 
-    public boolean execute(String sql, Object... params) {
+    public boolean execute(String sql, Object... params) throws SQLException {
         return processConnection(sql, PreparedStatement::execute, params);
     }
 
-    public int executeUpdate(String sql, Object... params) {
+    public int executeUpdate(String sql, Object... params) throws SQLException {
         return processConnection(sql, PreparedStatement::executeUpdate, params);
     }
 
-    public List<T> executeQuery(String sql, ObjectMapper<T> objectMapper, Object... params) {
+    public <T> List<T> executeQuery(String sql, ObjectMapper<T> objectMapper, Object... params) throws SQLException {
         return processConnection(sql, ps -> objectMapper.get(ps.executeQuery()), params);
     }
 
--- src/ru/javawebinar/basejava/storage/SqlStorage.java
+++ src/ru/javawebinar/basejava/storage/SqlStorage.java
@@ -11,31 +11,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SqlStorage implements Storage {
-    public final SqlHelper<Resume> sqlHelper;
+    public final SqlHelper sqlHelper;
+
+    public final SqlHelper.ObjectMapper<Resume> resumeMapper = rs -> {
+        List<Resume> list = new ArrayList<>();
+        while (rs.next()) {
+            list.add(new Resume(rs.getString("uuid").trim(), rs.getString("full_name")));
+        }
+        return list;
+    };
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
-        sqlHelper = new SqlHelper<>(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
+        sqlHelper = new SqlHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
     }
 
     @Override
     public void clear() {
-        sqlHelper.execute("DELETE FROM resume");
+        try {
+            sqlHelper.execute("DELETE FROM resume");
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 
     @Override
     public Resume get(String uuid) {
-        return sqlHelper.executeQuery("SELECT * FROM resume r WHERE r.uuid=?", rs -> {
-            if (!rs.next()) {
+        try {
+            List<Resume> result = sqlHelper.executeQuery("SELECT * FROM resume r WHERE r.uuid=?", resumeMapper, uuid);
+            if (result.size() == 0) {
                 throw new NotExistStorageException(uuid);
             }
-            return List.of(new Resume(uuid, rs.getString("full_name")));
-        }, uuid).get(0);
+            return result.get(0);
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 
     @Override
     public void update(Resume r) {
-        if (sqlHelper.executeUpdate("UPDATE resume r SET full_name=? WHERE r.uuid=?", r.getFullName(), r.getUuid()) == 0) {
-            throw new NotExistStorageException(r.getUuid());
+        try {
+            if (sqlHelper.executeUpdate("UPDATE resume r SET full_name=? WHERE r.uuid=?", r.getFullName(), r.getUuid()) == 0) {
+                throw new NotExistStorageException(r.getUuid());
+            }
+        } catch (SQLException e) {
+            throw new StorageException(e);
         }
     }
 
@@ -43,31 +62,40 @@ public class SqlStorage implements Storage {
     public void save(Resume r) {
         try {
             sqlHelper.execute("INSERT INTO resume (uuid, full_name) VALUES (?,?)", r.getUuid(), r.getFullName());
-        } catch (StorageException e) {
+        } catch (SQLException e) {
             throw new ExistStorageException(r.getUuid());
         }
     }
 
     @Override
     public void delete(String uuid) {
-        if (sqlHelper.executeUpdate("DELETE FROM resume r WHERE r.uuid=?", uuid) == 0) {
-            throw new NotExistStorageException(uuid);
+        try {
+            if (sqlHelper.executeUpdate("DELETE FROM resume r WHERE r.uuid=?", uuid) == 0) {
+                throw new NotExistStorageException(uuid);
+            }
+        } catch (SQLException e) {
+            throw new StorageException(e);
         }
     }
 
     @Override
     public List<Resume> getAllSorted() {
-        return sqlHelper.executeQuery("SELECT * FROM resume r ORDER BY r.uuid, r.full_name", rs -> {
-            List<Resume> list = new ArrayList<>();
-            while (rs.next()) {
-                list.add(new Resume(rs.getString("uuid").trim(), rs.getString("full_name")));
-            }
-            return list;
-        });
+        try {
+            return sqlHelper.executeQuery("SELECT * FROM resume r ORDER BY r.uuid, r.full_name", resumeMapper);
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 
     @Override
     public int size() {
-        return getAllSorted().size();
+        try {
+            return sqlHelper.executeQuery("SELECT count(*) FROM resume", rs -> {
+                rs.next();
+                return List.of(rs.getInt(1));
+            }).get(0);
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 }
